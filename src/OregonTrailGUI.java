76c6c5c22c3f47b49bb3bdf2082dc4feeaca6a0a
Ken Zhu <k-zhu@onu.edu>
2022-04-19 00:41:44 -0400

    gui frame updated and menu bar added

--- src/OregonTrailGUI.java
+++ src/OregonTrailGUI.java
@@ -2,35 +2,38 @@ import javax.swing.*;
 
 public class OregonTrailGUI {
 
+    private JPanel MainPanel;
+    private JPanel IMGPanel;
+    private JPanel BottomPanel;
     private JPanel GeneralPanel;
-    private JTextField thisAreaWillBeTextField;
+    private JTextField StoryTextField;
     private JLabel ImageLabel;
-    private JMenu menu1;
-    private JMenuItem menu1Item1;
 
     public static void main(String[] args) {
-        OregonTrailGUI game = new OregonTrailGUI();
+        JFrame frame = new JFrame();
+        frame.setContentPane(new OregonTrailGUI().MainPanel);
+        frame.setTitle("The Oregon Trail -- Remake");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+
+        JMenuBar menuBar = new JMenuBar();
+        menuBar.setVisible(true);
+        frame.setJMenuBar(menuBar);
+
+        JMenu menu1 = new JMenu("Menu 1");
+        menuBar.add(menu1);
+        JMenu menu2 = new JMenu("Menu 2");
+        menuBar.add(menu2);
+
+        JMenuItem menu1Item1 = new JMenuItem("Item 1");
+        menu1.add(menu1Item1);
+        JMenuItem menu2Item1 = new JMenuItem("Item 1");
+        menu2.add(menu2Item1);
     }
 
     //Create application
     public OregonTrailGUI(){
-        initialize();
-        if(true) {
-            ImageLabel.setIcon(new javax.swing.ImageIcon("src/assets/images/TestImage1.png"));
-        }
-    }
-
-    //initialize frame contents
-    private void initialize() {
-        JFrame frame = new JFrame();
-        frame.setTitle("The Oregon Trail -- Remake");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        JMenuBar menu = new JMenuBar();
-        frame.setJMenuBar(menu);
-        menu1 = new JMenu("menu1");
-        menu.add(menu1);
-
-        menu1.add(menu1Item1);
+        ImageLabel.setIcon(new javax.swing.ImageIcon("src/assets/images/TestImage1.png"));
     }
 }
