7d11e6311bbabb3fa2b830dcb1ee67aa830434c6
a-al-olimat <a-al-olimat@onu.edu>
2022-04-23 18:10:40 -0400

    finished RandomEvent class

--- src/RandomEvent.java
+++ src/RandomEvent.java
@@ -2,30 +2,85 @@ import javax.swing.*;
 
 public class RandomEvent {
     private final java.util.Random rand = new java.util.Random();
-    private int happiness = 0;
-    //initialize random event variables: type (good/bad), happiness
-    //if neutral --> 50/50 of event happening
-    //if happiness < 25 --> 75% chance of bad event, 25% chance of good
-    //if happiness >75 --> 75% chance of good event, 25% chance of bad
+    private int happiness;
+    private int type;
 
-
-
-    //initialize RandomEvent constructor taking in happiness
-
-    //method to determine good or bad event (using happiness, needs 3 scenarios)
-
-
-    //depending on good or bad, generate new random number to determine scenario
-
-    //call that method in the RandomEvent.generate() which returns a string form of what random event
-    //occurs to the main class
-
-    public RandomEvent(int happiness) {
-        this.happiness = happiness;
-
+    RandomEvent (int happiness) {
+        int mood = eventChance(happiness);
     }
 
+   int eventChance (int happiness) {
+       if (happiness < 75 && happiness > 25) {
+           return 1; //1=neutral
+       } else if (happiness >= 75) {
+           return 2; //2 is happy
+       } else {
+           return 0; //0 is sad
+       }
+   }
 
+    boolean eventType (int mood){
+        int temp;
+        boolean isGood;
+        if (mood == 1){
+           temp = rand.nextInt(2); //0 is bad 1 is good
+            if (temp == 1)
+            {
+                isGood = true;
+            }
+            else {
+                isGood = false;
+            }
+        }
+        else if (mood == 2){
+            temp = rand.nextInt(4);
+            if(temp == 0){
+                isGood = false;
+            }
+            else{
+                isGood = true;
+            }
+        }
+        else{
+            temp = rand.nextInt(4);
+            if(temp == 0){
+                isGood = true;
+            }
+            else
+            {
+                isGood = false;
+            }
+        }
+        return isGood;
+    }
 
+    String eventName(){
+        boolean isGood = eventType(eventChance(happiness));
+        int temp;
+        if(isGood == true){
+            temp = rand.nextInt(3);
+            if(temp == 0){
+                return "encounterTraveler"; //player encounters another traveler
+            }
+            else if(temp == 1){
+                return "smallStream"; //player encounters
+            }
+            else{
+                return "wagonFound";
+            }
+        }
+        else{
+            temp = rand.nextInt(3);
+            if(temp == 0){
+                return "injury";
+            }
+            else if (temp == 1){
+                return "wagonDamage";
+            }
+            else {
+                return "illness";
+            }
+        }
+    }
 }
 
