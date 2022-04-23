582f7f5caca5558060054010040505aebe330fdb
a-al-olimat <a-al-olimat@onu.edu>
2022-04-23 17:02:01 -0400

    random event tennis

--- src/RandomEvent.java
+++ src/RandomEvent.java
@@ -4,10 +4,16 @@ public class RandomEvent {
     private final java.util.Random rand = new java.util.Random();
     private int happiness = 0;
     //initialize random event variables: type (good/bad), happiness
+    //if neutral --> 50/50 of event happening
+    //if happiness < 25 --> 75% chance of bad event, 25% chance of good
+    //if happiness >75 --> 75% chance of good event, 25% chance of bad
+
+
 
     //initialize RandomEvent constructor taking in happiness
 
     //method to determine good or bad event (using happiness, needs 3 scenarios)
+
 
     //depending on good or bad, generate new random number to determine scenario
 
@@ -15,8 +21,11 @@ public class RandomEvent {
     //occurs to the main class
 
     public RandomEvent(int happiness) {
+        this.happiness = happiness;
 
     }
+
+
 
 }
 
