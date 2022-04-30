8ccc1354d790407c7459e2ea2dc682eb0c8e1efb
a-al-olimat <a-al-olimat@onu.edu>
2022-04-30 10:43:04 -0400

    added shop image

---
+++ src/Location.java
@@ -0,0 +1,23 @@
+/**
+ * The location class is used to track the location of the character/party during
+ * their journey from Missouri to Nebraska (Ash Hollow). This object will hold
+ * the name of the location the party is at in the game and the location will change
+ * depending on how much distance they have traveled.
+ */
+
+
+    /*Fort Bridger //wyoming --
+    Fort Kearney //nebraska --
+    Fort Laramie //wyoming --
+    Fort Hall //idaho --
+    Fort Boise //idaho --
+    Fort Vancouver //wyoming --
+
+    Blue //kansas
+    Wakarusa //kansas
+    Kansas //kansas
+    Big Blue //nebraska
+    Little Blue //Nebraska
+    Vermilion // ??
+    Platte River //Nebraska*/
+
