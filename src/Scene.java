764db7bfbec3df1413a69840f30731a08ec43a78
PJBeans <phillip236.po@gmail.com>
2022-05-02 06:28:34 -0400

    scene merged into gui class

--- src/Scene.java
+++ src/Scene.java
@@ -8,13 +8,17 @@ import java.util.ArrayList;
 import javax.swing.Timer;
 
 public class Scene extends JDialog {
+    private boolean sceneIsLoaded = false;
+    private boolean chainScenes = false;
+    ArrayList<String> sceneToRead = new ArrayList<>();
+    ArrayList<String> arrayOfScenes = new ArrayList<>();
     private JPanel contentPane;
     private JButton buttonOK;
     private JButton buttonCancel;
     private JTextArea storyTextArea;
     private JLabel imageLabel;
     private JButton continueButton;
-    private final SceneManager sceneMan = new SceneManager(continueButton,storyTextArea,imageLabel,this);
+    //private final SceneManager sceneMan = new SceneManager(continueButton,storyTextArea,imageLabel,this);
 
     public Scene() {
         this.setTitle("Scene");
@@ -31,14 +35,14 @@ public class Scene extends JDialog {
         continueButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                sceneMan.continueScene();
+                continueScene();
             }
         });
         storyTextArea.addKeyListener(new KeyAdapter() {
             @Override
             public void keyPressed(KeyEvent e) {
                 if(e.getKeyChar()=='c') {
-                    sceneMan.continueScene();
+                    continueScene();
                 }
             }
         });
@@ -46,29 +50,62 @@ public class Scene extends JDialog {
             @Override
             public void keyPressed(KeyEvent e) {
                 if (e.getKeyChar()=='c') {
-                    sceneMan.continueScene();
+                    continueScene();
                 }
             }
         });
     }
-    public void loadScene(String scene) {
-        sceneMan.loadScene(scene);
-        this.pack();
-        this.setVisible(true);
-    }
-
-    public void loadScene(String scene, String currentDate) {
-        sceneMan.loadScene(scene, currentDate);
-        this.pack();
-        this.setVisible(true);
+    //Load scene. Saves to global variable to avoid reopening textfile.
+    public void loadScene(String sceneName) {
+        if (!sceneIsLoaded) {
+            sceneToRead = ReadText.readScene(sceneName);
+            imageLabel.setIcon(new javax.swing.ImageIcon("src/assets/images/"+sceneName+".png"));
+            sceneIsLoaded = true;
+            continueButton.setVisible(true);
+            continueScene();
+            this.pack();
+            this.setVisible(true);
+        }
+        else {
+            System.out.println("SceneManager.java: Attempted to load scene " + sceneName + ", but another scene is loaded. Unload the scene first.");
+        }
     }
 
     public void chainLoadScene(ArrayList<String> arrayOfScenes) {
-        sceneMan.chainLoadScene(arrayOfScenes);
+        chainLoadScene(arrayOfScenes);
         this.pack();
         this.setVisible(true);
     }
+    //Continue reading a scene.
+    int readTextCounter = 0;
+    public void continueScene() {
+        try {
+            storyTextArea.setText(sceneToRead.get(readTextCounter));
+            readTextCounter++;
+        }
+        //Exception thrown if end of file is reached. Unload the scene.
+        catch (Exception e) {
+            if (!chainScenes) {unloadScene(true);}
+            else {
+                //chainScenesCounter++;
+                unloadScene(false);
+                //continueChainScene();
+            }
+        }
+    }
+    //Unload the scene by setting all values back to default.
+    public void unloadScene(boolean destroyWindow) {
+        storyTextArea.setText("");
+        imageLabel.setIcon(null); //We can set a default image here as well
+        readTextCounter = 0;
+        sceneIsLoaded = false;
+        continueButton.setVisible(false);
+        chainScenes = false;
+        sceneToRead.clear();
+        if (destroyWindow) {closeSceneWindow();}
 
+
+    }
     public void closeSceneWindow() {
         dispose();
     }
